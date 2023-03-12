927a42e56b4fbee5be41d92d015c80e684cfb8df
christiandda <christian.d.d.a@gmail.com>
2023-03-12 19:11:58 +0100

    update

---
+++ src/scripts/orders.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import os
+import sys
+from utils import recommend
+from PIL import Image
+import pickle
+import requests
+from pathlib import Path
+from PIL import Image
+import requests
+from io import BytesIO
+import glob
+import io
+import codecs
+from streamlit_option_menu import option_menu
+from scripts import un_based_rate
+from scripts import un_based_feat
+from scripts import sup_id
+from scripts import about
+
+PROJECT_ROOT = os.path.abspath(os.path.join(
+                  os.path.dirname(__file__), 
+                  os.pardir)
+)
+data_folder = (PROJECT_ROOT + "/" + "data")
+scripts_folder = (PROJECT_ROOT + "/" + "scripts")
+files_folder = (scripts_folder + "/" + "files")
+saved_models_folder = (data_folder + "/" + "saved_models")
+raw_data = (data_folder + "/" + "_raw")
+processed_data = (data_folder + "/" + "processed")
+images = (PROJECT_ROOT + "/" + "images")
+
+
+
+
+def drivers_info():
+    #Add the cover image for the cover page. Used a little trick to center the image
+             # To display the header text using css style
+
+    st.title('**We add orders**')
+    st.markdown(""" <style> .font {
+        font-size:40px ; font-family: 'Cooper Black'; color: #FF9633;} 
+        </style> """, unsafe_allow_html=True)
+    st.write("We add orders and get a return about which driver will do it")
