637fb1f2a9e3205b0ba5f98aa7f2fc5c5f98d2fd
christiandda <christian.d.d.a@gmail.com>
2023-03-13 14:04:59 +0100

    update

--- src/scripts/heatmap.py
+++ src/scripts/heatmap.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import os
 import sys
-
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 from PIL import Image
 import pickle
 import requests
@@ -13,8 +15,21 @@ import glob
 import io
 import codecs
 from streamlit_option_menu import option_menu
+import folium
+from streamlit_folium import st_folium, folium_static
+from folium.plugins import MarkerCluster
 
+
+from folium.plugins import HeatMapWithTime
 from utils import formulas
+
+
+import seaborn as sns
+
+
+from geopy.geocoders import Nominatim
+
+
 
 
 PROJECT_ROOT = os.path.abspath(os.path.join(
@@ -41,3 +56,28 @@ def heatmap_info():
         font-size:40px ; font-family: 'Cooper Black'; color: #FF9633;} 
         </style> """, unsafe_allow_html=True)
     st.write("We can see in real time the HOT (more busy) areas")
+
+
+
+    # Load the dataframes
+    shop_df = formulas.df_shops()
+    orders_df = formulas.df_orders()
+
+    # Merge the two dataframes on the restaurant_id column
+    merged_df = pd.merge(shop_df, orders_df, left_on='id', right_on='restaurant_id')
+    st.dataframe(merged_df)
+
+
+    # Load data with latitude and longitude columns
+    df = merged_df
+
+    # Create map
+    m = folium.Map(location=[40.4168, -3.7038], zoom_start=15)
+
+    # Add markers
+    heat_data = [[row["latitude"], row["longitude"]] for index, row in df.iterrows()]
+    from folium.plugins import HeatMap
+    HeatMap(heat_data).add_to(m)
+
+    # Display map
+    folium_static(m)
